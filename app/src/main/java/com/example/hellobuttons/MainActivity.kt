e752e32f4bc0562080b715afbc3047849e9de15b
Andrew Gray <agray@captechconsulting.com>
2020-05-29 17:20:41 -0400

    Add wiring for button to fire off a function when clicked

--- app/src/main/java/com/example/hellobuttons/MainActivity.kt
+++ app/src/main/java/com/example/hellobuttons/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.hellobuttons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun increment(view: View) {
+        // increment local count variable
     }
 }
