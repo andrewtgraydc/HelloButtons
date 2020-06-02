7cffee2483a088b4e3a89778794694f4c5db7319
Andrew Gray <agray@captechconsulting.com>
2020-06-01 21:17:36 -0400

    Store counter and have button increment

--- app/src/main/java/com/example/hellobuttons/MainActivity.kt
+++ app/src/main/java/com/example/hellobuttons/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.hellobuttons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +17,14 @@ class MainActivity : AppCompatActivity() {
 
     fun increment(view: View) {
         // increment local count variable
+        counter++
+
+        Log.i("increment1", "counter: $counter")
+
+        val counterLabel = findViewById<TextView>(R.id.textView).apply {
+            Log.i("increment2", "counter: $counter")
+
+            setText(counter.toString())
+        }
     }
 }
